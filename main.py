00b5ae70cafdf69faf0d2dfbeae0f88978ccd037
Phylosius <hei.phylosius@gmail.com>
2025-11-21 17:40:13 +0300

    chore: add salt

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    print("Hello from ci-py!")
+class Wallet:
+    def __init__(self, couleur, taille):
+        self.couleur = couleur
+        self.taille = taille
+        self.vola = 0
+        self.isOpen = True
+        self.isLost = False
 
-if __name__ == "__main__":
-    main()
+    def addVola(self, amount):
+        self.vola += amount
+
+    def getVola(self, amount):
+        self.vola -= amount
+
+    def checkVola(self, amount):
+        return self.vola
+    
+    def isLost(self):
+        return self.isLost
